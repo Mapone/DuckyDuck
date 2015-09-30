8450664399322080c2d40a8176c87c38eb392f13
BorisLehachoir <jeanmouldeux@gmail.com>
2015-09-30 21:43:36 +0200

    firstgame

--- sfmlTest.cpp
+++ sfmlTest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <string>
+#include <string> //to_String
+#include <stdlib.h> //rand
 #include <SFML/Graphics.hpp>
 
 using namespace std;
@@ -10,6 +11,10 @@ int main()
     ////////////INITIALISATION//////////
     ////////////////////////////////////
 
+    const int LARGEUR_FENETRE = 800;
+    const int HAUTEUR_FENETRE = 800;
+    const int TAILLE_HUD = 16;
+    const int TAILLE_CARRE = 10;
 
 //----------------------Fenetre----------------------
 //----------------------------------------------------
@@ -17,12 +22,12 @@ int main()
     settings.antialiasingLevel = 8;
 
     //Ouverture de la fenêtre principale
-    sf::RenderWindow window(sf::VideoMode(800, 800), "DuckyDuck",sf::Style::Default, settings);
+    sf::RenderWindow window(sf::VideoMode(LARGEUR_FENETRE, HAUTEUR_FENETRE), "DuckyDuck",sf::Style::Default, settings);
     //Limite à 60fps
     window.setFramerateLimit(60);
 
-//----------------------Police------------------------
-//----------------------------------------------------
+//----------------Police et textes -----------------------
+//--------------------------------------------------------
 
     sf::Font fontOldSchool;
     if (!fontOldSchool.loadFromFile("gamefont.ttf"))
@@ -31,7 +36,7 @@ int main()
     sf::Text staticScore;
     staticScore.setFont(fontOldSchool);
     staticScore.setString("Nombre de points:");
-    staticScore.setCharacterSize(16);
+    staticScore.setCharacterSize(TAILLE_HUD);
     staticScore.setColor(sf::Color::White);
     staticScore.setPosition(400,0);
 
@@ -41,6 +46,13 @@ int main()
     score.setCharacterSize(16);
     score.setColor(sf::Color::White);
     score.setPosition(700,0);
+
+    int randomX = rand() % (LARGEUR_FENETRE-TAILLE_CARRE);
+    int randomY = rand() % (HAUTEUR_FENETRE-TAILLE_CARRE);
+
+    sf::RectangleShape randomRect(sf::Vector2f(TAILLE_CARRE,TAILLE_CARRE));
+    randomRect.setFillColor(sf::Color::Red);
+    randomRect.setPosition(randomX,randomY);
 
     while (window.isOpen())
     {
@@ -55,9 +67,17 @@ int main()
         ++i_score;
         score.setString(to_string(i_score));
 
+        if(i_score%20==0)
+        {
+            randomX = rand() % (LARGEUR_FENETRE-TAILLE_CARRE);
+            randomY = rand() % (HAUTEUR_FENETRE-TAILLE_CARRE)+TAILLE_HUD;
+            randomRect.setPosition(randomX,randomY);
+        }
+
         window.clear();
         window.draw(staticScore);
         window.draw(score);
+        window.draw(randomRect);
         window.display();
     }
 
