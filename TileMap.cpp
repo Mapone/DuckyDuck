20d83efff2da9f1b80aaabbbddb7810aa2f435bd
BorisLeHachoir <jeanmouldeux@gmail.com>
2015-10-09 12:27:47 +0200

    tilemap0.1

--- TileMap.cpp
+++ TileMap.cpp
@@ -1,17 +1,3 @@
-/**************************************
-JEU ALAKON
-**************************************/
-
-#include <iostream>
-#include <string> //to_String
-#include <stdlib.h> //rand
-#include <SFML/Graphics.hpp>
-#include <time.h>
-
-using namespace std;
-using namespace sf;
-
-
 class TileMap : public sf::Drawable, public sf::Transformable
 {
 public:
@@ -35,7 +21,6 @@ public:
 
                 // on en déduit sa position dans la texture du tileset
                 int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
-                cout << tu << " = " << tileNumber << " modulo " << m_tileset.getSize().x << "/" << tileSize.x << endl;
                 int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
 
                 // on récupère un pointeur vers le quad à définir dans le tableau de vertex
@@ -74,78 +59,3 @@ private:
     sf::VertexArray m_vertices;
     sf::Texture m_tileset;
 };
-
-
-
-
-
-
-
-
-
-int main()
-{
-    ////////////////////////////////////
-    ////////////INITIALISATION//////////
-    ////////////////////////////////////
-
-    const int LARGEUR_FENETRE = 800;
-    const int HAUTEUR_FENETRE = 600;
-    const int TAILLE_HUD = 16;
-    const int TAILLE_CARRE = 20;
-    const int TIME_GAME = 120;
-
-//----------------------Fenetre----------------------
-//----------------------------------------------------
-    ContextSettings settings;
-    settings.antialiasingLevel = 8;
-
-    //Ouverture de la fenêtre principale
-    RenderWindow window(VideoMode(LARGEUR_FENETRE, HAUTEUR_FENETRE), "DuckyDuck",Style::Default, settings);
-    //Limite à 60fps
-    window.setFramerateLimit(60);
-
-//----------------Police et textes -----------------------
-//--------------------------------------------------------
-
-    Font fontOldSchool;
-    if (!fontOldSchool.loadFromFile("gamefont.ttf"))
-        cout << "#ERROR: Erreur lors du chargement de la police \"gamefont.tff\" " << endl;
-
-    int level[] = { 
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 
-    1,0,0,0,0,0,0,0,0,1,1,1,1,1,0, 
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 
-    0,0,3,4,0,0,0,2,2,2,2,0,0,2,2, 
-    0,0,5,6,0,0,0,0,0,0,0,0,0,0,0, 
-    0,0,5,6,0,0,0,0,0,0,0,0,0,0,0, 
-    0,0,5,6,0,0,0,0,0,0,0,0,0,0,0, 
-    7,7,7,7,7,7,7,7,7,7,7,7,7,7,7}; 
-
-      TileMap map;
-    if (!map.load("tileset1.png", sf::Vector2u(25, 25), level, 15, 13))
-        cout << "#ERROR: Erreur lors du chargement du tileset" << endl;
-
-
-
-
-    while (window.isOpen())
-    {
-        Event event;
-        while (window.pollEvent(event))
-        {       
-            if (event.type == Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(map);
-        window.display();
-    }
-    return 0;
-}
