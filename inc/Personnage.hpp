71439a525e369da30e52b9eefb470115876201f3
Mapone <pierreraballand@gmail.com>
2015-11-17 16:21:01 +0100

    gardons doxy à jour

--- inc/Personnage.hpp
+++ inc/Personnage.hpp
@@ -95,14 +95,88 @@ public:
 	*/
 	void flipLeft();
 
-
+	/**
+	* \fn unsigned int getCurrentKill() const
+	* \brief Getter de CurrentKill
+	*
+	* Récupère le nombre de kill fait par le personnage
+	* sur le niveau en cours
+	*
+	* \return Le nombre de kill
+	*/
 	unsigned int getCurrentKill() const;
+
+	/**
+	* \fn unsigned int getCurrentScore() const
+	* \brief Getter de CurrentScore
+	*
+	* Récupère le score fait par le personnage
+	* sur le niveau en cours
+	*
+	* \return Le score
+	*/
 	unsigned int getCurrentScore() const;
+
+	/**
+	* \fn unsigned int getTotalKill() const
+	* \brief Getter de TotalKill
+	*
+	* Récupère le nombre de kill fait par le personnage
+	* sur la totalité des niveaux
+	*
+	* \return Le nombre de kill total
+	*/
 	unsigned int getTotalKill() const;
+
+	/**
+	* \fn unsigned int getTotalScore() const
+	* \brief Getter de TotalScore
+	*
+	* Récupère le  score fait par le personnage
+	* sur la totalité des niveaux
+	*
+	* \return Le score total
+	*/
 	unsigned int getTotalScore() const;
+
+	/**
+	* \fn void setCurrentKill(unsigned int k)
+	* \brief Setter de CurrentKill
+	*
+	* Initialise le nombre de kill
+	*
+	* \param k : valeur d'initialisation
+	*/
 	void setCurrentKill(unsigned int k);
+
+	/**
+	* \fn void setCurrentScore(unsigned int s)
+	* \brief Setter de CurrentScore
+	*
+	* Initialise le score
+	*
+	* \param s : valeur d'initialisation
+	*/
 	void setCurrentScore(unsigned int s);
+
+	/**
+	* \fn void setTotalKill(unsigned int k)
+	* \brief Setter de TotalKill
+	*
+	* Initialise le nombre de total de kill
+	*
+	* \param k : valeur d'initialisation
+	*/
 	void setTotalKill(unsigned int k);
+
+	/**
+	* \fn void setTotalScore(unsigned int s)
+	* \brief Setter de TotalScore
+	*
+	* Initialise le score total
+	*
+	* \param s : valeur d'initialisation
+	*/
 	void setTotalScore(unsigned int s);
 
 
@@ -113,7 +187,7 @@ private:
 	bool _lookRight; /** Direction D/G vers ou le personnage est touné */
 	unsigned int _currentScore; /** Score temporaire du personnage*/
 	unsigned int _currentKill; /** Nombre d'enemis tué temporaire du personnage */
-	unsigned int _totalScore; /** Score total du personnage*/
+	unsigned int _totalScore; /** Score total du personnage */
 	unsigned int _totalKill; /** Nombre total d'ennemis tués */
 };
 
